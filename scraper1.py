ca4fa157c2fb5334fe6f1ce58db7ccc0cd5b8b51
Carcagi <car.car.as3@gmail.com>
2022-09-23 10:01:51 -0500

    Removed browser specifics, using xvfb with chrome webdriver

--- scraper1.py
+++ scraper1.py
@@ -1,35 +1,53 @@
+from pyvirtualdisplay import Display
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+#from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-import time, re, sys
-options = Options()
-options.headless = True
-s=Service('/usr/local/bin/chromedriver')
-d = webdriver.Chrome(service=s, options=options)
-d.get('https://muisca.dian.gov.co/WebRutMuisca/DefConsultaEstadoRUT.faces')
+from dotenv import load_dotenv
+import time, re, sys, os
+#import psycopg
 
+display = Display(visible=0, size=(1920, 1080))
+display.start()
+
+d = webdriver.Chrome()
+d.get('https://muisca.dian.gov.co/WebRutMuisca/DefConsultaEstadoRUT.faces')
 nit = sys.argv[1]
-def get_combos(nitNumber=''):
-    d.find_element(By.ID, "vistaConsultaEstadoRUT:formConsultaEstadoRUT:numNit").send_keys(str(nitNumber))
-    d.find_element(By.ID, "vistaConsultaEstadoRUT:formConsultaEstadoRUT:btnBuscar").click()
+company_id = sys.argv[2]
+
+def get_nitinfo(nitNumber='', company_id=''):
+
+    nit_field = d.find_element(By.ID, "vistaConsultaEstadoRUT:formConsultaEstadoRUT:numNit")
+    nit_field.send_keys(str(nitNumber))
+
+    submit_btn =  d.find_element(By.ID, "vistaConsultaEstadoRUT:formConsultaEstadoRUT:btnBuscar")
+    submit_btn.click()
+
+    name = d.find_element(By.ID, "vistaConsultaEstadoRUT:formConsultaEstadoRUT:razonSocial")
     
     try:
-        razon = d.find_element(By.ID, "vistaConsultaEstadoRUT:formConsultaEstadoRUT:razonSocial")
-        razon_social = razon.text
+        name = d.find_element(By.ID, "vistaConsultaEstadoRUT:formConsultaEstadoRUT:razonSocial")
+        company_name = name.text
     except:
-        razon_social = 'Not Found!'
+        company_name = ''
 
     try:
         status = d.find_element(By.ID, "vistaConsultaEstadoRUT:formConsultaEstadoRUT:estado")
-        estado = status.text
+        company_status = status.text
     except:
-        estado = 'Not Found!'
+        company_status = ''
 
-    print('Razon social: ', razon_social)
-    print('Estado: ', estado)
-    
     d.quit()
-    
 
-get_combos(nit)        
+    print('Razon social: ', company_name)
+    print('Estado: ', company_status)    
+
+def save_nitinfo(company_name, company_status, company_id):
+    with psycopg.connect("dbname=test user=postgres") as conn:
+        cur.execute("""
+            INSERT INTO companie_dian_validated (company_id, company_name, company_status)
+            VALUES (%s, %s, %s);
+            """,
+            (company_id, company_name, company_status))
+
+get_nitinfo(nit)    
+display.stop()    
